a446bbca0e9e6b92af94b3bfadd638669e36ea86
CicloM <CicloM@I2-PC12>
2019-11-07 09:58:40 +0100

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,59 @@
+/* 
+ * Copyright 2019 Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrian Bueno Olmedo <adrian.bueno.alum@iescamp.es>
+ */
+public class Main {
+
+    public static final Locale LCL = new Locale("en", "EN");
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(LCL);
+
+    public static void main(String[] args) {
+//          Variable
+        boolean procesoOK = false;
+        double n;
+//          Bucle
+
+        do {
+            try {
+                System.out.printf("El alumno ha APROBADO con......:");
+                n = SCN.nextDouble();
+                if (procesoOK = n >= 5 && n <= 10) {
+                    procesoOK = true;
+                    System.out.printf("El alumno ha APROBADO con......:%.2f"
+                            + " nota%n", n);
+                } else {
+                    System.out.println("ERROR: Entrada incorrecta.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("ERROR: Entrada incorrecta.");
+
+            } finally {
+                SCN.nextLine();
+            }
+        } while (!procesoOK);
+        System.out.println("END");
+    }
+}
